9cb9681753d80ee5feea04c718dd915371ba99c2
Monicaa1520 <monicaarangaraj@gmail.com>
2025-06-06 17:09:24 +0530

    day 1 task

---
+++ daily1/SumOfDigits.java
@@ -0,0 +1,22 @@
+package addon;
+
+	import java.util.Scanner;
+
+	public class SumOfDigits {
+
+		 public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter number: ");
+		        int number = scanner.nextInt();
+		        int result = sumOfDigits(number);
+		        System.out.println("Sum of digits: " + result);
+		        scanner.close();
+		    }
+		    public static int sumOfDigits(int n) {
+		        if (n == 0)
+		            return 0;
+		        return n % 10 + sumOfDigits(n / 10);
+		    }
+		}
+
+
